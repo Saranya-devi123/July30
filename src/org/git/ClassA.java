015823e52796c471ba17e8664dc003dd395d58a5
Saranya-devi123 <saranyadevi1081@gmail.com>
2025-07-30 13:33:29 +0530

    Done

---
+++ src/org/git/ClassA.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class ClassA {
+	
+	private void employeeId() {
+		System.out.println(" employee id : 001");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		ClassA c = new ClassA();
+		c.employeeId();
+		
+		
+	}
+
+}
